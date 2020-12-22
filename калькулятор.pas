c4180d77f48083162b17a696a1c92768422a60a7
fraer-maker <76464638+fraer-maker@users.noreply.github.com>
2020-12-22 08:04:13 +0300

    Add files via upload

---
+++ калькулятор.pas
@@ -0,0 +1,19 @@
+﻿program calcus;
+uses crt;
+var a,b,c:integer;
+d:string;
+begin
+clrscr;
+writeln('введите первое число');
+readln(a);
+writeln('знак +, -, *, /');
+readln(d);
+writeln('введите второе число');
+readln(c);
+if d=('+') then b:=a+c;
+if d=('-') then b:=a-c;
+if d=('*') then b:=a*c;
+if d=('/') then b:=a div c;
+writeln('ответ: ',b);
+readln;
+end.
